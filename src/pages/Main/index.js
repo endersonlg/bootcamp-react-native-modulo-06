f8d77b16be8d2d61d7a752fbea20a4d8b5be1fff
endersonlg <endersonluciog@gmail.com>
2020-03-10 19:02:49 -0300

    descarte

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,26 +1,10 @@
 import React from 'react';
-
-import Icon from 'react-native-vector-icons/MaterialIcons';
-
 import { View } from 'react-native';
 
-import { Container, Form, Input, SubmitButton } from './styles';
+import { Container } from './styles';
 
 export default function Main() {
-  return (
-    <Container>
-      <Form>
-        <Input
-          autoCorrect={false}
-          autoCapitalize="none"
-          placeholder="Adicionar usuário"
-        />
-        <SubmitButton>
-          <Icon name="add" size={20} color="#FFF" />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+  return <Container />;
 }
 
 Main.navigationOptions = {
