84af7eab2915bf2ed90c5e7a64eff4f1e4234d3a
endersonlg <endersonluciog@gmail.com>
2020-03-10 19:22:02 -0300

    11 - Acessando API do Github
    Aula feita ate 3:00

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { View } from 'react-native';
-
 import { Container, Form, Input, SubmitButton } from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <Form>
-        <Input
-          autoCorrect={false}
-          autoCapitalize="none"
-          placeholder="Adicionar usuário"
-        />
-        <SubmitButton>
-          <Icon name="add" size={20} color="#FFF" />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+export default class Main extends Component {
+  state = {
+    newUser: '',
+    users: [],
+  };
+
+  handleAddUser = () => {
+    console.tron.log(this.state.newUser);
+  };
+
+  render() {
+    const { users, newUser } = this.state;
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuário"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+          />
+          <SubmitButton onPress={this.handleAddUser}>
+            <Icon name="add" size={20} color="#FFF" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
 
 Main.navigationOptions = {
