6e191e4d5ec5862bd249fbc1a3593210ccbe35b7
endersonlg <endersonluciog@gmail.com>
2020-03-11 01:03:52 -0300

    11 - Acessando API do Github

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-
+import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
 
 import { Container, Form, Input, SubmitButton } from './styles';
 
@@ -10,8 +11,22 @@ export default class Main extends Component {
     users: [],
   };
 
-  handleAddUser = () => {
-    console.tron.log(this.state.newUser);
+  handleAddUser = async () => {
+    const { users, newUser } = this.state;
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+    });
+
+    Keyboard.dismiss();
   };
 
   render() {
@@ -25,6 +40,8 @@ export default class Main extends Component {
             placeholder="Adicionar usuário"
             value={newUser}
             onChangeText={text => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
           />
           <SubmitButton onPress={this.handleAddUser}>
             <Icon name="add" size={20} color="#FFF" />
